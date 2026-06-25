6be274a184c5e0dee21d96c816a86b15c6f4152f
JesusMunoz415 <jesusmunozmartinez608@gmail.com>
2026-06-25 01:28:06 -0600

    mejorar diseño del avatar

--- src/PerfilJugador.jsx
+++ src/PerfilJugador.jsx
@@ -297,7 +297,7 @@ const PerfilJugador = ({ jugadorId, onLogout }) => {
         
 
         {/* Avatar ocultable */}
-      <div className="avatar-ocultar-movil" style={{ marginLeft: '40px' }}>
+      <div className="avatar-ocultar-movil" style={{ marginLeft: '30px' }}>
          <AvatarEditor 
             key={`editor-atleta-${perfil.id}`}
             jugadorId={perfil.id} 
